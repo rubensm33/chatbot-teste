13bdce6fdf45250afa3ff85a9848069ba153c699
rubensm33 <rubensmarcelo31@gmaIl.com>
2023-02-02 13:12:34 -0300

    Formatação para código limpo

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import ApplicationBuilder, Application, CommandHandler, ContextTypes, ConversationHandler, MessageHandler,filters
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 # Enable logging
 logging.basicConfig(
@@ -8,14 +8,23 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
-    await update.message.reply_text("Hello there, My name is FolzeckGroup Bot, type /help to open the functionalities of our system")
+
+async def start(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    """_Start function from project"""
+    await update.message.reply_text(
+        "Hello there, My name is FolzeckGroup Bot, type /help to open the functionalities of our system"
+    )
 
 def main() -> None:
     """Run the bot."""
-    app = Application.builder().token("5890227974:AAFI_vdRD7GXSE3Iy4aiE6GF6vFAhMt_J3w").build()
+    app = (
+        Application.builder()
+        .token("5890227974:AAFI_vdRD7GXSE3Iy4aiE6GF6vFAhMt_J3w")
+        .build()
+    )
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
